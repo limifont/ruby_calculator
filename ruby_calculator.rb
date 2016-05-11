a45c4c271af8aaa08b33bcefcd8ddd62d65b237b
Lindsey Font <lindsey.font@gmail.com>
2016-05-11 12:07:03 -0600

    Pseudocode for basic calculator

--- ruby_calculator.rb
+++ ruby_calculator.rb
@@ -1,3 +1,20 @@
+# create / edit a file
+# stage the file - git add
+# make a commit
+# push to a github repo
+
 puts "Welcome!"
 puts "This is the Ruby Calculator :)\n"
 puts "Enter your first number:"
+
+# get the first number 
+puts "Enter your operator"
+# get the operator
+puts "Enter the second number"
+# get the second number
+
+# figure out how to do the math on the two numbers
+# output the result to the user
+
+# do all the bonuses
+
