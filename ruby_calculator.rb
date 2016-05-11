cd3c01743b6156006b151387321c694ec0c1bf0e
Lindsey Font <lindsey.font@gmail.com>
2016-05-11 17:29:56 -0600

    takes input and loops

--- ruby_calculator.rb
+++ ruby_calculator.rb
@@ -3,18 +3,86 @@
 # make a commit
 # push to a github repo
 
-puts "Welcome!"
-puts "This is the Ruby Calculator :)\n"
-puts "Enter your first number:"
 
-# get the first number 
-puts "Enter your operator"
-# get the operator
-puts "Enter the second number"
-# get the second number
+def num
+	calculation = []
+	puts "Enter a number"
+	num = gets
+
+	while true
+
+		if num.strip.to_f < 0 || num.strip.to_f > 0
+			puts 'valid'
+			calculation << num.strip.to_f
+			return num
+		elsif num.strip == '0'
+			puts 'valid'
+			calculation << num.strip.to_f
+			return num
+		else
+			puts 'Not valid'
+			puts "Enter a valid number"
+			num = gets
+		end
+	end
+end
+
+
+def get_operator
+	operators_arr = ['+', '-', '*', '/']	
+	puts "Enter your operator"
+	op = gets.strip
+
+	while true
+		if operators_arr.include? op
+			puts "Valid"
+			return op
+		else
+			puts "Not valid"
+			puts "Enter a valid operator"
+			op = gets.strip	
+		end
+	end
+end
+
+def last_num
+	puts "Enter a number"
+	last_num = gets.strip.to_f
+end
+
+
+# displays result
+def result (first, second, op)
+	first.send(op, second)
+end
+
+
+puts "Welcome!"
+
+while true
+	puts "This is the Ruby Calculator :)\n"
+	puts "Enter what you would like to calculate:"
+
+	num
+
+	get_operator
+
+	num
+
+		# puts "The result of #{first_num} #{get_operator} #{last_num} is:"
+		# puts result(first_num, last_num, get_operator)
+end 
+
+
+
+
+
+
+
 
 # figure out how to do the math on the two numbers
 # output the result to the user
 
 # do all the bonuses
+
 
